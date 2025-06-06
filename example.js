c54e0a047d4aa08fa9ffc63028a094826e075277
23047fs <23047fs@hvhs.school.nz>
2025-06-06 02:28:53 +0000

    Jun 6, 2025, 2:28 AM

---
+++ example.js
@@ -0,0 +1,30 @@
+/********************************
+Name of task: Rental Car 2
+Author: Finley
+Date: Term 2
+********************************/
+console.log("Running car_rentals_2.js");
+
+//Variables
+let carSELECTED
+
+//Arrays
+
+
+/********************************
+Main code
+********************************/
+function autoSTART() {
+
+}
+
+function start() {
+
+}
+
+/********************************
+Functions
+********************************/
+function car0() {
+    carSELECTED = 0
+}
